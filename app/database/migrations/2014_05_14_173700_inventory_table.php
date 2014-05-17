9067dcfee23a4fd1e62728b20747083648b6f69e
Sajid <sajed1990@gmail.com>
2014-05-18 02:52:40 +0600

    Create product

--- app/database/migrations/2014_05_14_173700_inventory_table.php
+++ app/database/migrations/2014_05_14_173700_inventory_table.php
@@ -17,8 +17,10 @@ class InventoryTable extends Migration {
             $table->integer("quantity");
             $table->text("comment")->nullable();
             $table->integer("user_id")->unsigned();
+            $table->integer("product_id")->unsigned();
             $table->timestamps();
             $table->foreign("user_id")->references("id")->on("users");
+            $table->foreign("product_id")->references("id")->on("products");
         });
 	}
 
@@ -29,7 +31,7 @@ class InventoryTable extends Migration {
 	 */
 	public function down()
 	{
-		//
+        Schema::drop("inventory_histories");
 	}
 
 }
---
+++ app/models/InventoryHistory.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 5/18/14
+ * Time: 2:39 AM
+ */
+
+class InventoryHistory {
+    public function product() {
+        return $this->belongsTo('Product');
+    }
+    public function user() {
+        return $this->belongsTo('User');
+    }
+} 
