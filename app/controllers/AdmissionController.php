533b4fa31dfcaf9620ee791138b5aebebd80669d
Rashad <rashad_rat@yahoo.com>
2014-06-09 23:33:13 +0600

    student create pr

--- app/controllers/AdmissionController.php
+++ app/controllers/AdmissionController.php
@@ -21,14 +21,18 @@ Class AdmissionController extends BaseController{
 
     public function save()
     {
+        $student_id = Input::get("student_ID");
         $name = Input::get("student_name");
         $father_name = Input::get("father_name");
         $mother_name = Input::get("mother_name");
+        $guardian_name = Input::get("guardian_name");
+        $birth = DateTime::createFromFormat('d-m-Y',Input::get("date_birth"));
         $gender = Input::get("gender");
         $nationality = Input::get("nationality");
         $religion = Input::get("religion");
         $address = Input::get("address");
         $contact_number = Input::get("contact_number");
+        $email_address = Input::get("email");
         $transport = Input::get("transport");
         $clazz = Input::get("clazz");
         $section = Input::get("section");
@@ -37,7 +41,43 @@ Class AdmissionController extends BaseController{
         $rsidclass = Input::get("rsidclass");
         $rsidsection = Input::get("rsidsection");
         $transportfee = Input::get("transportfee");
-
-
-}
+        $hasEntry = Student::find($student_id);
+        if($hasEntry){
+            return array('status' => 'error', 'message' => 'Student exists!');
+        }
+        $student = new Student();
+        $student->name = $name;
+        $student->father_name = $father_name;
+        $student->mother_name = $mother_name;
+        $student->guardian_name = $guardian_name;
+        $student->date_of_birth = $birth;
+        $student->gender = $gender;
+        $student->nationality = $nationality;
+        $student->religion = $religion;
+        $student->address = $address;
+        $student->contact_number = $contact_number;
+        $student->email = $email_address;
+        if($transport == 'No'){
+            $student->hasTransport = false;
+        }
+        else{
+            $student->hasTransport = true;
+        }
+        $student->transport_cost = $transportfee;
+        $student->clazz = $clazz;
+        $student->shift = $shift;
+        if($shift == 'Yes'){
+            $student->shift = $shift;
+        }
+        $student->section = $section;
+        if($rsidn){
+            $student->has_rid = true;
+        }
+        else{
+            $student->has_rid = false;
+        }
+        $student->rid = $rsidn;
+        $student->save();
+        return array('status' => 'success', 'message' => 'Student has been saved successfully.');
+    }
 }
